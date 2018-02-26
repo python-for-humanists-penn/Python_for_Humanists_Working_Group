d047451153ef50c41afcb9177884e3dd9eb49a21
Florian's GitHub account <florianb@sas.upenn.edu>
2018-02-26 12:23:36 -0500

    Add files via upload

---
+++ 2_26_2018/dict_homework.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+"""
+Dictionary Exercise from Chapter 5 of Automate the Boring Stuff...
+
+
+Create an initial dictionary and store it in birthdays 
+
+1. You can see if the entered name exists as a key in the dictionary with the `in` keyword 
+2. If the name is in the dictionary, you access the associated value using square brackets
+3. If not, you can add it using the same square bracket syntax combined with the assignment operator
+4. When you run this program, it will look like this:
+
+Enter a name: (blank to quit)
+Alice
+Apr 1 is the birthday of Alice
+Enter a name: (blank to quit)
+Eve
+I do not have birthday information for Eve
+What is their birthday?
+Dec 5
+Birthday database updated.
+Enter a name: (blank to quit)
+Eve
+Dec 5 is the birthday of Eve
+Enter a name: (blank to quit)
+"""
